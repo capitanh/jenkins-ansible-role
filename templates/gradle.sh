1b4edd582cf3beab432d6823c9a206b47c4a4245
Luis Novo <luis.novo@gmail.com>
2017-10-02 23:43:46 +0200

    Added gradle install

---
+++ templates/gradle.sh
@@ -0,0 +1,2 @@
+export GRADLE_HOME={{dev_tools_dir}}/gradle-{{gradle_version}}
+export PATH=$GRADLE_HOME/bin:$PATH
--- templates/maven.sh
+++ templates/maven.sh
@@ -1,2 +1,2 @@
-export M2_HOME={{maven_install_dir}}/apache-maven-{{maven_version}}
+export M2_HOME={{dev_tools_dir}}/apache-maven-{{maven_version}}
 export PATH=$M2_HOME/bin:$PATH
